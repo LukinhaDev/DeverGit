0deca29584c568308dffc87e1ea977e9dc6d9f07
Lucas Couto Neves <lucascouto2211@gmail.com>
2024-11-27 12:34:01 -0300

    Git Atualizado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
     <h1>Cruzeiro Maior de Minas!!!</h1>
-    <h4>Git Funcionando!</h4>
+    <h4>Git Funcionando!!</h4>
     <h3>#paimmelhorprofessor</h3>
 </body>
 
