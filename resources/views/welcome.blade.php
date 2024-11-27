3138fabdde13001bb8136ee04be4220436ef5409
Lucas Couto Neves <lucascouto2211@gmail.com>
2024-11-27 11:54:08 -0300

    comit dever

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+</head>
+
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <h1>Cruzeiro Maior de Minas!!!</h1>
+    <h4>Git Funcionando!</h4>
+    <h3>#paimmelhorprofessor</h3>
+</body>
+
+</html>
